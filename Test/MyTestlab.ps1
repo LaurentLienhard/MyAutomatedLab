3c5e43fd4574492af19e022d3f722768d1588d20
LaurentLienhard <laurent.lienhard@outlook.com>
2022-09-23 10:12:21 +0200

    COmmit

---
+++ Test/MyTestlab.ps1
@@ -0,0 +1,17 @@
+$LabName = "MyTestLab"
+$DomainName = "contoso.local"
+$DomainAdministrator = "Administrator"
+$DomainPassword = "Passw0rd"
+$VmPath = Join-Path -Path "C:\LabSources\Labs" -ChildPath $LabName
+$VirtualEngine = "HyperV"
+
+New-LabDefinition -Name $LabName -DefaultVirtualizationEngine $VirtualEngine -VmPath $VmPath
+
+Add-LabVirtualNetworkDefinition -Name $LabName -AddressSpace 10.1.0.0/16
+
+Add-LabMachineDefinition -Name DC1 -Memory 2GB -OperatingSystem 'Windows Server 2022 Datacenter' -Roles RootDC -DomainName contoso.com -Network $LabName -ToolsPath "$labSources\Tools" 
+#Add-LabMachineDefinition -Name Client1 -Memory 4GB -OperatingSystem 'Windows 11 Enterprise' -DomainName contoso.com -Network $LabName -ToolsPath "$labSources\Tools" 
+
+Install-Lab
+
+Show-LabDeploymentSummary -Detailed
